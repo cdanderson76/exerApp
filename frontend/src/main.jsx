1cf2ad937ab06b854e8a2d8833752d4b2db6308d
Clifford Anderson <138369806+cdanderson76@users.noreply.github.com>
2025-12-10 12:38:14 -0600

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client';
-import { WorkoutContextProvider } from './context/WorkoutContext.jsx';
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <WorkoutContextProvider>
-      <App />
-    </WorkoutContextProvider>
-  </StrictMode>,
+    <App />
+  </StrictMode>
 )
